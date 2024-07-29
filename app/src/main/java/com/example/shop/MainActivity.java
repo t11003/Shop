d7c0984c34f92dc4999dd2b115c305ea095858fc
t11003 <t11003t11003@gmail.com>
2024-07-29 11:24:10 +0800

    clickMap功能完成

--- app/src/main/java/com/example/shop/MainActivity.java
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.shop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void clickMap(View view){
+        Intent i = new Intent();
     }
 }
